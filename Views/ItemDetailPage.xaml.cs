ab4cd4e12f5dd327020f33638583fa8d5ac2a6fe
JesusHernandez25 <151281682+JesusHernandez25@users.noreply.github.com>
2023-12-04 19:55:17 -0600

    Add files via upload

---
+++ Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TomeTender.ViewModels;
+using Xamarin.Forms;
+
+namespace TomeTender.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
