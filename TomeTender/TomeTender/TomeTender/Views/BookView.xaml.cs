c28715875b359a4a4622c9e9db87d0097e928fa5
Zamos026 <152237254+Zamos026@users.noreply.github.com>
2024-01-02 06:04:18 -0500

    Adds a page to view book info. Also adds a tabbed page for BookView and BookAvailability

---
+++ TomeTender/TomeTender/TomeTender/Views/BookView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TomeTender.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BookView : ContentPage
+    {
+        public BookView()
+        {
+            InitializeComponent();
+        }
+        //Back Button
+        //private async void OnBackClicked(object sender, EventArgs e)
+        //{
+        //    await Navigation.PopAsync();
+        //}
+        //Home Button
+        private async void OnHomeClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
