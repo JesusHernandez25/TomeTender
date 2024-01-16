25105a5898c95b4ce01082fba439f4a7b166d302
Jesus Hernandez <hernandezj801@DUPAGE.EDU>
2024-01-15 18:58:05 -0600

    complete

---
+++ TomeTender/TomeTender/TomeTender/Views/HomePage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomeTender;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace TomeTender
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        async void GoToLibraryListPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LibraryListPage());
+        }
+
+        async void GoToEventsPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EventsPage());
+        }
+
+    }
+
+}
+
+    
+
+
+
---
+++ TomeTender/TomeTender/TomeTender/Views/LibraryListPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomeTender;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TomeTender
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LibraryListPage : ContentPage
+	{
+		public ObservableCollection<string> LibraryList { get; set; }
+        public ObservableCollection<string> LibraryList2 { get; set; }
+        public ObservableCollection<string> LibraryList3 { get; set; }
+        public ObservableCollection<string> LibraryList4 { get; set; }
+
+        public LibraryListPage ()
+		{
+			InitializeComponent ();
+            {
+               
+
+                LibraryList = new ObservableCollection<string> { "Reading Oasis" };
+                LibraryList2 = new ObservableCollection<string> { "Silent Stacks Society"  };
+                LibraryList3 = new ObservableCollection<string> { "Bookworm Retreat" };
+                LibraryList4 = new ObservableCollection<string> { "Wisdom Vault" };
+
+
+                LibraryListView.ItemsSource = LibraryList;
+                LibraryListView2.ItemsSource = LibraryList2;
+                LibraryListView3.ItemsSource = LibraryList3;
+                LibraryListView4.ItemsSource = LibraryList4;
+            }
+        }
+
+	
+		
+	}
+}
